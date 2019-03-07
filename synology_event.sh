84fe3082aab00e2d24773bc9eec0efe9bc098464
iml885203 <iml885203@gmail.com>
2019-03-07 23:26:00 +0800

    fix command error

--- synology_event.sh
+++ synology_event.sh
@@ -2,5 +2,5 @@
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/config
 if [ "$synology_notify" == "yes" ]
 then
-/usr/syno/bin/synonotify DDNSFail $1
+/usr/syno/bin/synonotify DDNSFail "$1"
 fi
